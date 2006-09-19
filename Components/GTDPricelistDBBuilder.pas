bdf0a6a072681beda0f3c953cd456981d64faf6e
djlyon <djlyon>
2006-09-19 02:19:26 +0000

    Initial revision

---
+++ Components/GTDPricelistDBBuilder.pas
@@ -0,0 +1,20 @@
+unit GTDPricelistDBBuilder;
+
+interface
+
+uses 
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs;
+
+type
+  TPricelistBuildFromDB = class(TFrame)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.DFM}
+
+end.
